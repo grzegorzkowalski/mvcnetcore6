5229df72485c2f2aa47f59a4153cc480ea5c3ce6
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-01-20 09:20:50 +0100

    pass json in request

--- Workshop_17_01_2023/Workshop_17_01_2023/Controllers/RoleController.cs
+++ Workshop_17_01_2023/Workshop_17_01_2023/Controllers/RoleController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using FilmDB.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using Newtonsoft.Json;
 
 namespace FilmDB.Controllers
 {
@@ -66,6 +69,67 @@ namespace FilmDB.Controllers
                 }
             }
             return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            var result = _roleManager.Roles.SingleOrDefault(x => x.Id == id);
+            if (result == null)
+            {
+                return View("Error");            
+            }
+            return View(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(IdentityRole role)
+        {
+            var result = await _roleManager.UpdateAsync(role);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult AddToRole(string errors)
+        {
+            var users =  _userManager.Users;
+            var roles =  _roleManager.Roles;
+            List<IdentityError> errorsList = new();
+            if (errors != null) 
+            {
+                errorsList = JsonConvert.DeserializeObject<List<IdentityError>>(errors);
+            }
+            
+            var userRoles = new UsersRolesViewModel()
+            {
+                Users = users,
+                Roles = roles,
+                Errors = errorsList
+            };
+
+            return View(userRoles);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToRolePost([FromForm(Name ="userID")] string UserID, [FromForm(Name = "roleName")] string RoleName)
+        {
+            var user = await _userManager.FindByIdAsync(UserID);
+            if (user != null)
+            {
+                var result = await _userManager.AddToRoleAsync(user, RoleName);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+          
+                else 
+                {
+                    List<IdentityError> errorList = result.Errors.ToList();
+                    string json = JsonConvert.SerializeObject(errorList);
+                    return RedirectToAction("AddToRole", new {errors = json});
+                }
+            }
+            return View("Error");
         }
     }
 }
---
+++ Workshop_17_01_2023/Workshop_17_01_2023/ViewModels/UsersRolesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FilmDB.ViewModels
+{
+    public class UsersRolesViewModel
+    {
+        public IQueryable<IdentityUser> Users { get; set; }
+        public IQueryable<IdentityRole> Roles { get; set; }
+        public List<IdentityError>? Errors { get; set; }
+    }
+}
