ca745bebd932ffcbb64899ae01a280c8b7304cf0
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-01-20 11:15:16 +0100

    api

---
+++ Workshop_17_01_2023/Workshop_17_01_2023/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using FilmDB.logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : Controller
+    {
+        private readonly FilmManager _filmManager;
+        public MovieController (FilmManager filmManager)
+        {
+            _filmManager = filmManager;
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var film = _filmManager.GetFilm(id);
+            return Json(film);
+        }
+    }
+}
