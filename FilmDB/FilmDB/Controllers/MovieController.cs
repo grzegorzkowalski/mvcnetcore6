09cee78a5db54bffccd1de563220da7aeebfdb41
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-01-27 12:43:05 +0100

    web api all

--- FilmDB/FilmDB/Controllers/MovieController.cs
+++ FilmDB/FilmDB/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
-﻿using FilmDB.Repositories;
+﻿using FilmDB.Models;
+using FilmDB.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmDB.Controllers
@@ -17,7 +18,59 @@ namespace FilmDB.Controllers
         public IActionResult Get(int id)
         {
             var film = _filmManager.GetFilm(id);
+            if (film == null) 
+            {
+                return NotFound();
+            }
             return Ok(film);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var films = _filmManager.GetFilms();
+            if (films.Count() == 0)
+            {
+                return NoContent();
+            }
+            return Ok(films);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] FilmModel filmModel)
+        {
+            _filmManager.AddFilm(filmModel);
+            return Ok();
+        }
+
+        //[HttpPost]
+        //public IActionResult Create2([FromBody] FilmModel filmModel)
+        //{
+        //    _filmManager.AddFilm(filmModel);
+        //    return CreatedAtAction(nameof(Create2), new { id = filmModel.ID }, filmModel);
+        //}
+
+        //[HttpPost]
+        //public IActionResult Create3([FromBody] FilmModel filmModel)
+        //{
+        //    _filmManager.AddFilm(filmModel);
+        //    var url = $"{HttpContext.Request.Host.Value}/api/movie/{filmModel.ID}";
+        //    return Created(url, "Success");
+        //}
+
+
+        [HttpPatch]
+        public IActionResult Update([FromBody] FilmModel filmModel)
+        {
+            _filmManager.UpdateFilm(filmModel);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _filmManager.RemoveFilm(id);
+            return NoContent();
         }
     }
 }
