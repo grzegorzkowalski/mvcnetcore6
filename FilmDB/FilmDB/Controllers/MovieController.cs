c8054ab735a7429635c073466b84fda180617aa7
Grzegorz Kowalski <grzegorzkowalski@yahoo.com>
2023-01-27 11:27:19 +0100

    webAPI start

---
+++ FilmDB/FilmDB/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using FilmDB.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+        private readonly FilmManager _filmManager;
+        public MovieController(FilmManager filmManager)
+        {
+            _filmManager = filmManager;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var film = _filmManager.GetFilm(id);
+            return Json(film);
+        }
+    }
+}
